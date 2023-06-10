8371d01ebac5e114112e9b8f6638691f477e5dfc
Maxim Bernatsky <maxsateon@yandex.ru>
2023-06-10 19:44:52 +0300

    feat:create pagination by using redux

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,10 +10,12 @@ import Skeleton from '../components/PizzaBlock/Skeleton';
 import Pagination from '../components/Pagination/Pagination';
 import { SearchContext } from '../App';
 
-import { setCategoryId } from '../redux/slices/filterSlice';
+import { setCategoryId, setCurrentPage } from '../redux/slices/filterSlice';
 
 function Home() {
-  const { categoryId, sort } = useSelector((state) => state.filter);
+  const { categoryId, sort, currentPage } = useSelector(
+    (state) => state.filter,
+  );
 
   const dispatch = useDispatch();
 
@@ -23,7 +25,7 @@ function Home() {
 
   const [isLoading, setIsLoading] = useState(true);
 
-  const [currentPage, setCurrentPage] = useState(1);
+  const onChangePage = (number) => dispatch(setCurrentPage(number));
 
   useEffect(() => {
     setIsLoading(true);
@@ -60,7 +62,7 @@ function Home() {
           ? [...new Array(9)].map((_, index) => <Skeleton key={index} />)
           : items.map((item) => <PizzaBlock key={item.id} {...item} />)}
       </div>
-      <Pagination onChangePage={(num) => setCurrentPage(num)} />
+      <Pagination currentPage={currentPage} onChangePage={onChangePage} />
     </div>
   );
 }
